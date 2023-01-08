e2680d2a14f51dcc9cb2cf387022b6b4885fbc1e
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-01-08 11:59:59 -0600

    bring in icons for genreal info section

--- src/components/currentWeatherUi.js
+++ src/components/currentWeatherUi.js
@@ -10,7 +10,7 @@ const displayWeather = async () => {
         weatherData.current_description,
         weatherData.current_temps.temp
     );
-    displayCurrentWeather(weatherData.current_temps)
+    displayCurrentWeather(weatherData.current_temps,weatherData.current_wind_speed)
 
 };
 
@@ -27,7 +27,24 @@ const displayGeneralInfo = (name,weather,temp) => {
 
 };
 
-const displayCurrentWeather = (currentTemps) => {
+const displayCurrentWeather = (currentTemps,wind) => {
+    const feelImg = document.getElementById('')
+    const feelTag = document.getElementById('feels_tag')
+    const humidImg = document.getElementById('')
+    const humidTag = document.getElementById('humidity-tag')
+    const highImg = document.getElementById('')
+    const highTag = document.getElementById('high-tag')
+    const lowImg = document.getElementById('')
+    const lowTag = document.getElementById('low-tag')
+    const windImg = document.getElementById('')
+    const windTag = document.getElementById('wind-speed')
+
+
+    feelTag.innerText = getFarenheight(currentTemps.feels_like)
+    humidTag.innerText = `${currentTemps.humidity}%`;
+    highTag.innerText = getFarenheight(currentTemps.temp_max)
+    lowTag.innerText = getFarenheight(currentTemps.temp_min)
+    windTag.innerText = `${wind}MPH`
     console.log(currentTemps)
 
 }
